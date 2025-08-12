198b738b8642446d8df0cbbae4400e806362b077
Gautam Mahajan <159855180+Gautam9981@users.noreply.github.com>
2025-08-12 19:11:07 +0530

    Update fedora.sh
    Signed-off-by: Gautam Mahajan <159855180+Gautam9981@users.noreply.github.com>

--- fedora.sh
+++ fedora.sh
@@ -228,11 +228,11 @@ sudo mkdir -p /mnt/dev
 sudo mkdir -p /mnt/proc
 sudo mkdir -p /mnt/sys
 sudo mkdir -p /mnt/usr
-echo "Mounting /dev, /proc, and /sys for installation environment..."
-mount --bind /dev /mnt/dev
-mount --bind /proc /mnt/proc
-mount --bind /sys /mnt/sys
-mount --bind /usr /mnt/usr
+echo "Mounting /dev, /proc, /sys, and /usr for installation environment..."
+sudo mount --bind /dev /mnt/dev
+sudo mount --bind /proc /mnt/proc
+sudo mount --bind /sys /mnt/sys
+sudo mount --bind /usr /mnt/usr
 
 
 echo "Installing Fedora minimal system with $de_group..."

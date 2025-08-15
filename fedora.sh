26b940e1c756e8fa2996a7f69392917a57518116
Gautam Mahajan <159855180+Gautam9981@users.noreply.github.com>
2025-08-15 05:41:17 +0000

    Update fedora.sh
    Signed-off-by: Gautam Mahajan <159855180+Gautam9981@users.noreply.github.com>

--- fedora.sh
+++ fedora.sh
@@ -295,7 +295,7 @@ for fs in sys dev proc run; do
 done
 
 echo "Installing Fedora minimal system with $de_group..."
-dnf install --installroot=/mnt --releasever=42 --setopt=install_weak_deps=False -y @core $de_group grub2-efi shim efibootmgr || \
+dnf install --installroot=/mnt --releasever=42 --setopt=install_weak_deps=False -y @core $de_group grub2-efi-x64 grub2-efi-modules shim efibootmgr || \
 dnf install --installroot=/mnt --releasever=42 -y @core $de_group grub2
 
 echo "Installing bootloader..."
